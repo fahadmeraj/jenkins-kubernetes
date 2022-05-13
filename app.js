a76502f97aad9fc06fb32c3a0ed060b91571aa95
root <root@1.140>
2022-05-13 04:48:18 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => res.send('Hello World!7'))
+
+var server = app.listen(3000, () => {
+    console.log("Listening on port " + server.address().port + "...");
+});
